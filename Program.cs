cb902ea5f80e8f220c7b445d6fb192ff9ffe0aba
Micha Ostapchuk <ostapchuk.mish@yandex.ru>
2022-09-17 10:21:29 +0300

    Реализовал сложение двух чисел

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 ﻿class Programm
 {
+
+    int X { get; set; }
+    int Y { get; set; }
+    
+    
     static void Main(string[] args)
     {
-        
-        Console.WriteLine("Здравствуй МИР!");
+       
+        Programm myProg = new Programm();
+        myProg.X = 1;
+        myProg.Y = 2;
+        int z = myProg.X + myProg.Y;
+
+        string result = $"Результат сложения числа {myProg.X} и {myProg.Y} равен {z}";     
+            
+        Console.WriteLine(result);
         Console.ReadLine(); 
     }
 }
