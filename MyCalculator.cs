570635ae53e55c4035cf78395e68550fe5b6aa51
Micha Ostapchuk <ostapchuk.mish@yandex.ru>
2022-09-17 10:33:24 +0300

    Создал класс MyCalculator и реализовал там два метода сложение и вычитание

---
+++ MyCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCode
+{
+    internal class MyCalculator
+    {
+        //public MyCalculator()
+        //{
+        //}
+
+        int X { get; set; }
+        int Y { get; set; }
+
+        int Result { get; set; }
+
+        int AddDigit(int a, int b) 
+        {
+            int z = a + b;
+            return z;
+        
+        }
+
+        int SubtractDigit(int a, int b)
+        {
+            int z = a - b;
+            return z;
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,9 @@
 ﻿class Programm
 {
-
-    int X { get; set; }
-    int Y { get; set; }
-    
-    
     static void Main(string[] args)
     {
        
-        Programm myProg = new Programm();
-        myProg.X = 1;
-        myProg.Y = 2;
-        int z = myProg.X + myProg.Y;
+        
 
         string result = $"Результат сложения числа {myProg.X} и {myProg.Y} равен {z}";     
             
